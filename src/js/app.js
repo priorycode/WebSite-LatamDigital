3d47f0efa637601bcd5604e62d012faddfb94e50
priorycode <jhordymondragon4@gmail.com>
2024-01-15 00:09:24 -0500

    corrección de animaciones

--- src/js/app.js
+++ src/js/app.js
@@ -11,15 +11,15 @@ btnClose.addEventListener("click", () => {
   body.classList.toggle("scroll-none");
 });
 
-const containerNav = document.querySelector(".container-nav");
-function borrarAnimacion() {
-  const nuevoAncho = window.innerWidth;
-  if (nuevoAncho < 768) {
-    containerNav.removeAttribute("data-aos", "fade-zoom-in");
-    containerNav.removeAttribute("data-aos-delay", "300");
-  } else {
-    containerNav.setAttribute("data-aos", "fade-zoom-in");
-    containerNav.setAttribute("data-aos-delay", "300");
-  }
-}
-let num = window.addEventListener("resize", borrarAnimacion);
+// const containerNav = document.querySelector(".container-nav");
+// function borrarAnimacion() {
+//   const nuevoAncho = window.innerWidth;
+//   if (nuevoAncho < 768) {
+//     containerNav.removeAttribute("data-aos", "fade-zoom-in");
+//     containerNav.removeAttribute("data-aos-delay", "300");
+//   } else {
+//     containerNav.setAttribute("data-aos", "fade-zoom-in");
+//     containerNav.setAttribute("data-aos-delay", "300");
+//   }
+// }
+// let num = window.addEventListener("resize", borrarAnimacion);
