1e26b6a0d1d9bc88d1ee2f14ecdeb2c8cf1b2cb8
priorycode <jhordymondragon4@gmail.com>
2024-01-15 00:03:56 -0500

    Avance de las paginas y add animation

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,25 @@
 const btnBar = document.querySelector(".btn-bar");
 const btnClose = document.querySelector(".btn-close");
 const nav = document.querySelector(".list-nav");
-const body = document.querySelector('[data-body]');
+const body = document.querySelector("[data-body]");
 btnBar.addEventListener("click", () => {
   nav.classList.toggle("mostrar");
-  body.classList.toggle('scroll-none')
+  body.classList.toggle("scroll-none");
 });
 btnClose.addEventListener("click", () => {
   nav.classList.toggle("mostrar");
-  body.classList.toggle('scroll-none')
+  body.classList.toggle("scroll-none");
 });
+
+const containerNav = document.querySelector(".container-nav");
+function borrarAnimacion() {
+  const nuevoAncho = window.innerWidth;
+  if (nuevoAncho < 768) {
+    containerNav.removeAttribute("data-aos", "fade-zoom-in");
+    containerNav.removeAttribute("data-aos-delay", "300");
+  } else {
+    containerNav.setAttribute("data-aos", "fade-zoom-in");
+    containerNav.setAttribute("data-aos-delay", "300");
+  }
+}
+let num = window.addEventListener("resize", borrarAnimacion);
