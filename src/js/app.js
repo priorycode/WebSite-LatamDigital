a0472a79863e895c2d2e85d2070c1cc211e11e79
priorycode <jhordymondragon4@gmail.com>
2024-01-14 19:03:11 -0500

    Pagina Nosotros Listo

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,12 @@
 const btnBar = document.querySelector(".btn-bar");
 const btnClose = document.querySelector(".btn-close");
 const nav = document.querySelector(".list-nav");
+const body = document.querySelector('[data-body]');
 btnBar.addEventListener("click", () => {
   nav.classList.toggle("mostrar");
+  body.classList.toggle('scroll-none')
 });
 btnClose.addEventListener("click", () => {
   nav.classList.toggle("mostrar");
+  body.classList.toggle('scroll-none')
 });
